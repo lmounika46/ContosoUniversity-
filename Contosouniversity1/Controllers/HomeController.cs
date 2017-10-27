19ce4695dded77cadd77dfb277e79f95e65a0a94
Neel Mallepally <nmallepally@smartbutton.com>
2017-10-26 22:31:48 -0400

    Captital letter

--- Contosouniversity1/Controllers/HomeController.cs
+++ Contosouniversity1/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Contosouniversity1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your Application Description Page.";
 
             return View();
         }
