6a4c68449c29d6de302a5fce2b5a6346260885a8
Neel Mallepally <nmallepally@smartbutton.com>
2017-10-27 23:23:57 -0400

    Added new featurs.

---
+++ Contosouniversity1/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contosouniversity1.Models
+{
+    public class Course
+    {
+    }
+}
