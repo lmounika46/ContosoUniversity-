f70550fec793db4ca72cbb3197c1fca0bff977a5
Neel Mallepally <nmallepally@smartbutton.com>
2017-11-17 22:38:44 -0500

    Create a new folder for entity

---
+++ Contosouniversity1/Data/SchoolContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contosouniversity1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contosouniversity1.Data
+{
+    public class SchoolContext :DbContext
+    {
+    }
+}
